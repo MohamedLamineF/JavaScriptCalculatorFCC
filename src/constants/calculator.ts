a24686c1161509e42b3b2c00564adac13d96781b
MohamedLamineF <fadimoh@hotmail.fr>
2024-12-19 17:48:14 +0100

    hot fix test

--- src/constants/calculator.ts
+++ src/constants/calculator.ts
@@ -1,3 +1,5 @@
-export const INITIAL_DISPLAY_VALUE = '0';
-export const DECIMAL_POINT = '.';
-export const OPERATORS = ['+', '-', '*', '/'] as const;
+export const INITIAL_DISPLAY_VALUE = "0";
+
+export const DECIMAL_POINT = ".";
+
+export const OPERATORS = ["+", "-", "*", "/"] as const;
