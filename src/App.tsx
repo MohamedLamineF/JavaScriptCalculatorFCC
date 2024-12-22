17004b1638f29ad0d33b9cc3ec50380b05e928bd
MohamedLamineF <fadimoh@hotmail.fr>
2024-12-22 23:44:09 +0100

    fix light mode

--- src/App.tsx
+++ src/App.tsx
@@ -11,12 +11,15 @@ function App() {
       <div className="relative h-screen">
         <Header isDark={isDark} onToggleTheme={() => setIsDark(!isDark)} />
         <div className="absolute inset-0">
-          <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"></div>
+          <div
+            className={`absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 ${
+              isDark
+                ? '[background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]'
+                : '[background:radial-gradient(125%_125%_at_50%_10%,#f5f5f5_40%,#63e_100%)]'
+            }`}
+          ></div>
         </div>
         <main className="container mx-auto px-4 py-8 mb-28"></main>
-        {/* <div className="relative z-10 flex h-full flex-col items-center justify-center px-4">
-          <div className="max-w-3xl text-center"></div>
-          </div> */}
       </div>
       <Footer />
     </>
